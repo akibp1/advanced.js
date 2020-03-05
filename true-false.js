0ab38bd4dde39154d406230e2d39a94fd38ec405
akibp1 <mda588825@gmail.com>
2020-03-05 20:15:01 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+/**jekno  sonkha/value jody 0 bade onno kichu hoi taile js 
+  seta true dorbe ar jody 0 hoi taile js false dorbe*/
+ const age = 0 ;
+ if (age > 4){
+     console.log("condition is true");
+ }
+ else{
+     console.log("condition is false");
+ }
+ /**jekno number ii truthy other thane 0 chara
+  * jekno string ii truthy other than empty string chara 
+  */
+
+/** jekno cmd ar ii value set kore na dile seta automatic undefined/false asbe */
+let name;
+console.log(name);
