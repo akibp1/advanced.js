0337987f9507dd11d95d1019f8554f271df870ab
akibp1 <mda588825@gmail.com>
2020-03-06 00:35:33 +0600

    undefined buijha gachi

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+                    //undefined
+/**1. value set na korle undefined dakhai
+
+
+let pakhi ;
+console.log(pakhi);*/
+
+/**2. Function likhchi kintu return ki korbe boli nai 
+function add (num1,num2){
+    console.log(num1+num2);
+    return
+}
+const result = add(12, 14);*/
+
+/**function ar parameter pass na korle undefined dakhai aiter somadhan holo defult  value set kore dite
+ * hoi
+ * function add (num1, num2) {
+ * console.log( num2);
+ * }
+ * const result = add(13,)
+ *console.log (result); 
+ */
+const premik = {name:"smart dude", phone:57349}
+console.log(premik.gf);
--- true-false.js
+++ true-false.js
@@ -1,3 +1,5 @@
+                                //False values
+
 /**jekno  sonkha/value jody 0 bade onno kichu hoi taile js 
   seta true dorbe ar jody 0 hoi taile js false dorbe*/
  const age = 0 ;
@@ -11,6 +13,18 @@
   * jekno string ii truthy other than empty string chara 
   */
 
-/** jekno cmd ar ii value set kore na dile seta automatic undefined/false asbe */
+/** jekno variable ar defined set kore na dile seta automatic undefined/false asbe */
 let name;
 console.log(name);
+/**null dile seta seta false value  */
+let name;
+console.log(name);
+
+/** NaN (not a number) aitaw akta false values */
+
+//detract false declare kore dile seta akta false value hobe 
+
+                                //Truthy values
+                                //false value chara sobi truthy value
+
+                                //like ('0'," ",[],)
